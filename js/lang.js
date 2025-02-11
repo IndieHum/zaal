554f4cc99ae0e5cf87d1a7e5583a62e12232d8cd
IndieHum <moein.fereyduni@gmail.com>
2025-02-11 15:42:55 +0330

    adding english prototype

--- js/lang.js
+++ js/lang.js
@@ -2,13 +2,13 @@ export function PageChange(event) {
   const SelectedLanguage = event.target.value;
   switch (SelectedLanguage) {
     case "fa":
-      if (window.location.href.includes("index-en.html"))
+      if (window.location.href.includes("/lang/en/"))
         window.location.assign("../../index.html")
       else window.location.reload();
       break;
     case "en":
-      if (!window.location.href.includes("index-en.html"))
-        window.location.assign("../lang/en/index-en.html")
+      if (!window.location.href.includes("/lang/en/"))
+        window.location.assign("../lang/en/index.html")
       else window.location.reload();
       break;
   }
